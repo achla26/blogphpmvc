56cb675f1962e05a3bd66d6bbd5a55d71ab06015
achla dhiman <achladhiman3@gmail.com>
2021-07-02 22:04:34 +0530

    initial commit

---
+++ application/views/about.php
@@ -0,0 +1,16 @@
+<?php
+    component('header');
+    component('navbar');
+?>
+<div class="container">
+    <div class="row header">
+        <h2>About Us</h2>
+    </div>
+    <div class="para">
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero</p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of cla ofessor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero</p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero</p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consecteturns.</p>
+    </div>   
+</div>
+<?php component('footer'); ?>
---
+++ application/views/components/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark  fixed-top ">
+  <div class="container">
+    <a class="navbar-brand" href="<?php echo BASEURL;?>">
+    <img src="<?php echo BASEURL;?>assets/images/logo.png">
+  </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor03">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item active">
+          <a class="nav-link " href="<?php echo BASEURL;?>">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+              
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo BASEURL;?>Contact">Contact Us</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo BASEURL;?>About">About Us</a>
+        </li>
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+
